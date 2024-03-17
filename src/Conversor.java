1c6a2d305bbf683547c4fd8a3fd5c366fc7c098f
Caio Henrique <caiohsilvamc@gmail.com>
2024-03-16 23:57:13 -0300

    Ajustando código

--- src/Conversor.java
+++ src/Conversor.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Conversor {
     public static void main(String[] args) throws Exception {
        
+        String binario;
+        int decimal;
+        int resto;
+        String hexadecimal = "";
+        char hexa[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         
         Scanner scan = new Scanner( System.in);
         int escolha;
@@ -16,12 +21,6 @@ public class Conversor {
 
         switch (escolha) {
             case 1:
-               String binario;
-               int decimal;
-               int resto;
-               String hexadecimal = "";
-               char hexa[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-
                System.out.println("Digite o número...");
                decimal = scan.nextInt(); 
 
@@ -37,40 +36,29 @@ public class Conversor {
             break;
 
             case 2:
-               String binario2;
-               int decimal2;
-               int resto2;
-               String hexadecimal2 = "";
-               char hexa2[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-
                System.out.println("Digite o número...");
-               binario2 = scan.next(); 
+               binario = scan.next(); 
 
-               decimal2 = Integer.parseInt(binario2, 2);
-               System.out.println("Em Decimal: " + decimal2);
+               decimal = Integer.parseInt(binario, 2);
+               System.out.println("Em Decimal: " + decimal);
 
-               while(decimal2 > 0) {
-                resto2 = decimal2 % 16;
-                hexadecimal2 = hexa2[resto2] + hexadecimal2;
-                decimal2 = decimal2/16;
+               while(decimal > 0) {
+                resto = decimal % 16;
+                hexadecimal = hexa[resto] + hexadecimal;
+                decimal = decimal/16;
                }
-               System.out.println("Em Hexadecimal: " + hexadecimal2);
+               System.out.println("Em Hexadecimal: " + hexadecimal);
             break;
 
             case 3:
-            
-            String binario3 = "";
-            int decimal3;
-            String hexadecimal3 = "";
-
             System.out.println("Digite o número...");
-            hexadecimal3 = scan.next(); 
+            hexadecimal = scan.next(); 
             
-            decimal3 = Integer.parseInt(hexadecimal3, 16);
-            System.out.println("Em Decimal: " + decimal3);
+            decimal = Integer.parseInt(hexadecimal, 16);
+            System.out.println("Em Decimal: " + decimal);
 
-            binario3 = Integer.toBinaryString(decimal3);
-            System.out.println("Em Binário: " + binario3);
+            binario = Integer.toBinaryString(decimal);
+            System.out.println("Em Binário: " + binario);
             break;
         
             default:
